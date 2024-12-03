f06d91f83f1734dd6466f329c769007610cb99f7
Hoskis <vip.izyavich@mail.ru>
2024-12-04 00:38:45 +0300

    initial commit

---
+++ src/main/java/org/example/finassistant/FinassistantApplication.java
@@ -0,0 +1,13 @@
+package org.example.finassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinassistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinassistantApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/properties.md
@@ -0,0 +1,6 @@
+# Файл .properties должен быть файлик в следующем формате
+>spring.application.name=finassistant
+spring.datasource.url=jdbc:postgresql://localhost:5432/test
+spring.datasource.username=postgres
+spring.datasource.password=postgres
+spring.datasource.driver-class-name=org.postgresql.Driver
